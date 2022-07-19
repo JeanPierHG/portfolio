2910483a7947513c48d4d58c7f8daaaad41d4ad6
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-19 11:02:32 -0500

    styles hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,18 +1,37 @@
 import Image from 'next/image'
 import styles from './Hero.module.css'
 import profile from '../../public/profile.png'
-
+import { VscGithub } from 'react-icons/vsc'
+import { BsLinkedin } from 'react-icons/bs'
+import { RiSendPlane2Line } from 'react-icons/ri'
 const Hero = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.ladoIzquierdo}>
         <Image
           objectFit='cover'
-          width={600}
+          width={700}
           height={800}
           src={profile}
           alt='mi-foto'
+          className={styles.picture}
         />
+        <div className={styles.redIcons}>
+          <a
+            href='https://github.com/JeanPierHG'
+            target={'_blank'}
+            rel='noopener noreferrer'
+          >
+            <VscGithub className={styles.github} />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/jeanfullstackdev/'
+            target={'_blank'}
+            rel='noopener noreferrer'
+          >
+            <BsLinkedin className={styles.linkedin} />
+          </a>
+        </div>
       </div>
       <div className={styles.ladoDerecho}>
         <div className={styles.header}>
@@ -20,11 +39,13 @@ const Hero = () => {
           <span>Full Stack Developer</span>
         </div>
         <p>
-          Desarrollador apasionado por el código,para mí es un placer haber
+          Desarrollador apasionado por el código, para mí es un placer haber
           elegido esta carrera, actualmente me encuentro en busquedad de mis
           primeras experiencias profesionales.
         </p>
-        <button>Descargar CV</button>
+        <button>
+          Descargar CV <RiSendPlane2Line className={styles.iconButtonSend} />
+        </button>
       </div>
     </div>
   )
